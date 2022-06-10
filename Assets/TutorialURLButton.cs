b487b46420687d8400e947cc22c2e80facefcf16
atozc <catoz@nevada.unr.edu>
2022-06-10 15:16:02 -0700

    Added Terminal Briefing Screen, help button, released new build

---
+++ Assets/TutorialURLButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialURLButton : MonoBehaviour
+{
+    [SerializeField] private string _TutorialURL;
+
+    public void OpenTutorial()
+    {
+        Application.OpenURL(_TutorialURL);
+    }
+}
