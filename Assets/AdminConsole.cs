7aac1d7ea31c76cfbababd6e02896ae83a0f26d3
atozc <catoz@nevada.unr.edu>
2022-09-29 16:32:54 -0700

    Testing script for admin console; new tutorial url

---
+++ Assets/AdminConsole.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AdminConsole : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://www.cse.unr.edu/~crystala/testFile.txt");
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+}
